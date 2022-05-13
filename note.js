f0b792040acb24c6c6261007459bfa0bd43ed6a8
signaling-office <87625878+signaling-office@users.noreply.github.com>
2022-05-13 17:41:28 +0900

    Create note.js

---
+++ note.js
@@ -0,0 +1,17 @@
+//jqueryで要素の座標
+width = $(window).width()/100;
+height = $(window).height()/100;
+
+var x = 50;
+var y = 50;
+$("#red-box").offset({left:x*width, top:y*height})
+
+
+//iframeの動的変更
+var preview = document.getElementById("preview");  
+var idoc = preview.contentDocument;  
+var html = "<html><body>hoge</body></html>";
+
+idoc.open();  
+idoc.write(html);  
+idoc.getElementsByClassName
