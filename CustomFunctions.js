0673eb30f6de83955082214e04451f314fa8b072
signaling-office <87625878+signaling-office@users.noreply.github.com>
2022-05-13 21:08:24 +0900

    Update CustomFunctions.js

--- CustomFunctions.js
+++ CustomFunctions.js
@@ -1,10 +1,49 @@
 // start
+function _start(code){
+  return 'async function main(){\n' + code + '};\n';
+}
+
 // turn
+function _turn(angle,classname){
+  return;
+}
+
 // go
+function _go(angle,classname){
+  return;
+}
+
 // move
+function _move(){
+  return;
+}
+
 // sleep
+function _sleep(){
+  return;
+}
+
 // resize
+function _resize(){
+  return;
+}
+
 // class
+function _class(){
+  return;
+}
+
 // get position
+function _get_position(){
+  return;
+}
+
 // set color
+function _set_color(){
+  return;
+}
+
 // run code
+function _run_code(){
+  return;
+}
